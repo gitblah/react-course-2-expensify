290b68e8b5e63e2a3fd394f4d7cf618d2fb97596
jb <jusb@iinet.net.au>
2018-09-08 10:00:10 +0800

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,14 +13,10 @@ import { Stream } from 'stream';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
-
 //store.dispatch(setTextFilter('water'));
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//const state = store.getState();
+//const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 //console.log(visibleExpenses);
 
 const jsx = (
