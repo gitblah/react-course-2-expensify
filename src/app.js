b01a18189fe1e0216fb2867a942547c7308a18ad
jb <jusb@iinet.net.au>
2018-09-14 19:20:08 +0800

    Setup production build and server -fix

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
-import './Styles/styles.scss';
+import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { Stream } from 'stream';
 
